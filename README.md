d374caec51fb625f958f62c17c354e2d8c55be11
James Leong <jyleong@sfu.ca>
2017-11-05 13:09:22 -0800

    initial commit of voiceminder server

---
+++ README.md
@@ -0,0 +1,57 @@
+# Voiceminder Server
+Repository the Voiceminder database and REST API
+
+### Environment Setup
+Install python3:
+```
+> brew upgrade python3
+```
+
+Install and configure virtualenv:
+```
+> pip install virtualenv
+> mkdir ~/Python_envs
+> cd ~/Python_envs
+> virtualenv -p /usr/local/Cellar/python3/3.6.2/bin/python3 voiceminder
+```
+
+# Using and Updating the Environment:
+Anytime you want to activate this environment:
+```
+> source ~/Python_envs/voiceminder/bin/activate
+```
+
+You will know if you have the environment activated or not from your
+command line prompt:
+```
+(voiceminder) >
+```
+
+All the required python packages are defined in the
+requirements.txt file. Use the `pip` command to install or
+upgrade. Remember you need to do this each time someone adds a required
+package to the project.
+```
+(voiceminder) > pip3 install -r requirements.txt
+```
+
+To add a required package to the project, use the `pip freeze` command:
+```
+(voiceminder) > pip3 freeze > requirements.txt
+```
+
+Run instructions
+install mysql
+make database voiceminder
+export environment variables:
+```
+(voiceminder) > source voiceminder.env
+(voiceminder) > python3 manage.py db upgrade
+(voiceminder) > python3 manage.py seed
+(voiceminder) > python3 run.py
+```
+
+install localtunnel via localtunnel.me
+launch in terminal
+> lt -s voiceminder -p 5000
+lauches api as https://voiceminder.localtunnel.me
---
+++ src/models.py
@@ -0,0 +1,46 @@
+# models.py
+# created by James L 07/07/2017
+
+# file for all our database related python objects, used for ORM
+
+from src.app_file import db
+
+class User(db.Model):
+    """The test case table in mera_db"""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    sleeprecord = db.relationship('SleepRecord', backref='sleeprecords', lazy='dynamic')
+
+
+    def __init__(self, name):
+        self.name = name
+
+class SleepRecord(db.Model):
+    """The test case table in sleeportant"""
+    __tablename__ = "sleeprecords"
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
+    sleepState = db.Column(db.String(60)) # GOOD, OKAY, BAD
+    startTime = db.Column(db.BIGINT)
+    endTime = db.Column(db.BIGINT)
+    fullTimeSlept = db.Column(db.Integer)
+    date = db.Column(db.BIGINT)
+    timeToSleep = db.Column(db.Integer)
+    timeWokenUp = db.Column(db.Integer)
+    durationWokenUp = db.Column(db.Integer)
+
+
+    def __init__(self, userId, sleepState, startTime, endTime, fullTimeSlept,
+                 date, timeToSleep, timeWokenUp, durationWokenUp):
+        self.userId = userId
+        self.sleepState = sleepState
+        self.startTime = startTime
+        self.endTime = endTime
+        self.fullTimeSlept = fullTimeSlept
+        self.date = date
+        self.timeToSleep = timeToSleep
+        self.timeWokenUp = timeWokenUp
+        self.durationWokenUp = durationWokenUp
