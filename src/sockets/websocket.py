8e6e1ecf6a42b708ca666edbbc2d8abb6139c071
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-11-20 06:59:19 -0800

    WIP onmessage websocket

--- src/sockets/websocket.py
+++ src/sockets/websocket.py
@@ -2,7 +2,6 @@ import uuid
 from tornado.websocket import WebSocketHandler
 from sockets.socket_instances import SocketInstances
 
-
 class WebSocket(WebSocketHandler):
     def open(self):
         self.id = str(uuid.uuid4())
@@ -11,18 +10,23 @@ class WebSocket(WebSocketHandler):
     def on_message(self, message):
         print("message: ", message)
         # name = getUserName(message)
+        # usermessage = getUsermessage(message)
+
         # search message for persons name/ or id to know who to send to
         for k, v in SocketInstances.socketStorage.items():
             print("id: {}, socket instance: {}".format(k,v))
             if k != self.id:
                 print("writing message")
+                #TODO refactor into usermessage
                 v.write_message(message)
         
         SocketInstances.setSocketIdByName(self.id, "june")
 
-        # destinationName = getDestinationName(message)
+        #TODO refactor into name
         destination = SocketInstances.getSocketInstanceByName("june")
         print("destination: ", destination)
+
+        #TODO refactor into usermessage
         destination.write_message("june said hey kevin")
         print("number of sockets in SocketInstances: ", len(SocketInstances.socketStorage))
 
