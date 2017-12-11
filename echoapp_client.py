59d332089c2947ace830218b3a566e1d2c8db81d
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-12-10 16:34:30 -0800

    updated echoapp client

--- echoapp_client.py
+++ echoapp_client.py
@@ -1,34 +1,61 @@
 import speech_recognition
 from gtts import gTTS
 import os
-def speak(incomingtext):
-	print(incomingtext)
-	tts = gTTS(text=incomingtext, lang='en')
-	tts.save("incomingtext.mp3")
-	os.system("mpg321 incomingtext.mp3")
 
 import websocket
 from threading import Thread
 import time
 import sys
 
+speaking = False
+
+def speak(incomingtext):
+    speaking = True
+    print(incomingtext)
+    tts = gTTS(text=incomingtext, lang='en')
+    tts.save("incomingtext.mp3")
+    os.system("mpg321 incomingtext.mp3")
+    speaking = False
+
+recognizer = speech_recognition.Recognizer()
+def listen():
+    with speech_recognition.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=500, phrase_time_limit=3000)
+
+    try:
+        # print(recognizer.recognize_sphinx(audio))
+        # return recognizer.recognize_sphinx(audio)
+        print(recognizer.recognize_google(audio))
+        return recognizer.recognize_google(audio)
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+    except speech_recognition.UnknownValueError:
+        print("Could not understand audio, trying again")
+    except speech_recognition.RequestError as e:
+        print("Recog Error; {0}".format(e))
+
+    return ""
 
 def on_message(ws, message):
-    print(message)
-
+    speak(message)
 
 def on_error(ws, error):
     print(error)
 
 def on_close(ws):
+    speak("closed")
     print("### closed ###")
 
 def on_open(ws):
-    def run(*args):
+    def deliver(*args):
         while True:
-            raw = input()
-            ws.send(raw)
-    runThread = Thread(target=run)
+            if not speaking:
+                raw = listen()
+                if raw:
+                    ws.send(raw)
+    runThread = Thread(target=deliver)
     runThread.daemon = False
     runThread.start()
 
