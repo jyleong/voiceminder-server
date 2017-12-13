8e94bc8bfec619a3900f1075a942d7f1db3f0bb5
James Leong <jyleong@sfu.ca>
2017-12-13 12:39:26 -0800

    terminate single thread complete

--- src/asynchronous/countdown.py
+++ src/asynchronous/countdown.py
@@ -6,16 +6,15 @@ class CountDown(threading.Thread):
     def __init__(self, method):
         threading.Thread.__init__(self)
         self.event = threading.Event()
-        self.count = 5
         # lambda method we must provide method arguemtsn to it
         self.method = method
 
     def run(self):
-        print("Starting countdown threading method")
-        while self.count > 0 and not self.event.is_set():
+        print("COUTNDOWN: Starting countdown threading method")
+        while not self.event.is_set():
+            print("COUNTDOWN: executing lambda method...")
             self.method()
-            time.sleep(1)
-            self.count -= 1
+            time.sleep(5)
             self.event.wait(1)
 
     def stop(self):
