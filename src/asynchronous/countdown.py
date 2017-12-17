8c0cc4aef0ad5603f9e74e1821f1df0447e4e77e
James Leong <jyleong@sfu.ca>
2017-12-17 15:43:15 -0800

    rebasing

--- src/asynchronous/countdown.py
+++ src/asynchronous/countdown.py
@@ -1,22 +1,37 @@
 import threading
 import time
 
-class CountDown(threading.Thread):
+class EventLoop(threading.Thread):
 
     def __init__(self, method, duration=5):
         threading.Thread.__init__(self)
         self.event = threading.Event()
-        # lambda method we must provide method arguemtsn to it
+        # lambda method we must provide method arguements to it
         self.method = method
         self.duration = duration
 
+    # this method doesn work if you call it
     def run(self):
-        print("COUTNDOWN: Starting countdown threading method")
+        print("EVENTLOOP: Starting eventloop threading method")
         while not self.event.is_set():
-            print("COUNTDOWN: executing lambda method...")
+            print("EVENTLOOP: executing lambda method...")
             self.method()
             time.sleep(self.duration)
             self.event.wait(1)
 
     def stop(self):
         self.event.set()
+
+class Countdown(threading.Thread):
+
+    def __init__(self, method, duration=5):
+        threading.Thread.__init__(self)
+        self.event = threading.Event()
+        # lambda method we must provide method arguements to it
+        self.method = method
+        self.duration = duration
+
+    def run(self):
+        print("COUNTDOWN: Starting countdown threading method")
+        time.sleep(self.duration)
+        self.method()
