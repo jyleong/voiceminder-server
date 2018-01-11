407149d9661a61f276884ede85222a0e4fe9bd19
James Leong <jyleong@users.noreply.github.com>
2018-01-11 13:51:26 -0800

    taking out unnecessary files, remove the confirm name event loop (#58) (#59)

--- src/manage.py
+++ src/manage.py
@@ -1,6 +1,5 @@
 # manage.py
 
-import os
 import datetime
 import time
 from random import randint
@@ -15,9 +14,6 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
-def findFullTimeSlept(endTime, startTime, timeToSleep, timeWokenUp, durationWokenUp):
-    fullTimeSlept = (endTime - startTime) - timeToSleep - timeWokenUp * durationWokenUp
-    return fullTimeSlept
 
 @manager.command
 def seed():
@@ -40,68 +36,6 @@ def seed():
     db.session.commit()
     print("Users created!")
     print("Seeding database for users sleep records")
-
-    # per user per date, make random entry
-    # random entry time to sleep in minutes, times woken up is int, duration woken up is minutes
-    # starttime, endtime are timestamps, fulltimesleep is F = (E-D) -A - BC
-    date1 = datetime.date(2017, 3, 1)
-    date2 = datetime.date(2017, 8, 21)
-    day = datetime.timedelta(days=1)
-    dateTemp = date1
-    while dateTemp <= date2:
-        print(dateTemp.strftime('%Y.%m.%d'))
-        randIndex1 = randint(0, 2)
-        randIndex2 = randint(0, 2)
-        randIndex3 = randint(0, 2)
-        randTimeDuration1 = randint(4,10)
-        randTimeDuration2 = randint(4,10)
-        randTimeDuration3 = randint(4,10)
-
-        currentDate = time.mktime(dateTemp.timetuple())
-
-        startTime1 = currentDate + randint(17, 20) * 3600
-        endTime1 = startTime1 + randTimeDuration1 * 3600
-
-        startTime2 = currentDate + randint(17, 20) * 3600
-        endTime2 = startTime2 + randTimeDuration2 * 3600
-
-        startTime3 = currentDate + randint(17, 20) * 3600
-        endTime3 = startTime3 + randTimeDuration3 * 3600
-
-
-        timeToSleep1 = randint(0,2400)
-        timeWokenUp1 = randint(0,4)
-        durationWokenUp1 = randint(0,60)
-
-        timeToSleep2 = randint(0, 2400)
-        timeWokenUp2 = randint(0, 4)
-        durationWokenUp2 = randint(0, 60)
-
-        timeToSleep3 = randint(0, 2400)
-        timeWokenUp3 = randint(0, 4)
-        durationWokenUp3 = randint(0, 60)
-
-
-        fullTimeSlept1 = findFullTimeSlept(endTime1, startTime1, timeToSleep1, timeWokenUp1, durationWokenUp1)
-        fullTimeSlept2 = findFullTimeSlept(endTime2, startTime2, timeToSleep2, timeWokenUp2, durationWokenUp2)
-        fullTimeSlept3 = findFullTimeSlept(endTime3, startTime3, timeToSleep3, timeWokenUp3, durationWokenUp3)
-        sleepRecord1 = models.SleepRecord(user1.id, sleepState=sleepStateList[randIndex1],
-                                          startTime=startTime1, endTime=endTime1, fullTimeSlept=fullTimeSlept1,
-                                          date=currentDate, timeToSleep=timeToSleep1, timeWokenUp=timeWokenUp1,
-                                          durationWokenUp=durationWokenUp1)
-        sleepRecord2 = models.SleepRecord(user2.id, sleepState=sleepStateList[randIndex2],
-                                          startTime=startTime2, endTime=endTime2, fullTimeSlept=fullTimeSlept2,
-                                          date=currentDate, timeToSleep=timeToSleep2, timeWokenUp=timeWokenUp2,
-                                          durationWokenUp=durationWokenUp2)
-        sleepRecord3 = models.SleepRecord(user3.id, sleepState=sleepStateList[randIndex3],
-                                          startTime=startTime3, endTime=endTime3, fullTimeSlept=fullTimeSlept3,
-                                          date=currentDate, timeToSleep=timeToSleep3, timeWokenUp=timeWokenUp3,
-                                          durationWokenUp=durationWokenUp3)
-        db.session.add_all([sleepRecord1, sleepRecord2, sleepRecord3])
-        db.session.flush()
-        db.session.commit()
-        dateTemp = dateTemp + day
-    print("all sleep records added")
     return
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -12,7 +12,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60))
     socketInstanceId = db.Column(db.String(60)) # to store the uuid that maps to a websocket instance
-    sleeprecord = db.relationship('SleepRecord', backref='sleeprecords', lazy='dynamic')
 
 
     def __init__(self, name):
@@ -23,31 +22,3 @@ class User(db.Model):
 
     def setSocketInstanceId(self, socketInstanceId):
         self.socketInstanceId = socketInstanceId
-
-class SleepRecord(db.Model):
-    """The test case table in sleeportant"""
-    __tablename__ = "sleeprecords"
-
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
-    sleepState = db.Column(db.String(60)) # GOOD, OKAY, BAD
-    startTime = db.Column(db.BIGINT)
-    endTime = db.Column(db.BIGINT)
-    fullTimeSlept = db.Column(db.Integer)
-    date = db.Column(db.BIGINT)
-    timeToSleep = db.Column(db.Integer)
-    timeWokenUp = db.Column(db.Integer)
-    durationWokenUp = db.Column(db.Integer)
-
-
-    def __init__(self, userId, sleepState, startTime, endTime, fullTimeSlept,
-                 date, timeToSleep, timeWokenUp, durationWokenUp):
-        self.userId = userId
-        self.sleepState = sleepState
-        self.startTime = startTime
-        self.endTime = endTime
-        self.fullTimeSlept = fullTimeSlept
-        self.date = date
-        self.timeToSleep = timeToSleep
-        self.timeWokenUp = timeWokenUp
-        self.durationWokenUp = durationWokenUp
